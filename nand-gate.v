9ce676866a191bf200ff73928dff086932a49b74
cyxabima <ukashaanwerali@gmail.com>
2025-11-09 16:43:02 +0500

    added:: universal-logic-gates

---
+++ nand-gate.v
@@ -0,0 +1,8 @@
+module nand_gate( 
+    input a, 
+    input b, 
+    output out );
+    
+    assign out = ~(a&b);
+
+endmodule
---
+++ nor-gate.v
@@ -0,0 +1,8 @@
+module nor_gate( 
+    input a, 
+    input b, 
+    output out );
+    
+    assign out = ~(a|b);
+
+endmodule
